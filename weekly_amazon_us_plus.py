4e451b403c8225999bd0a5df12e567a017333e8c
jongpard <jongpard@gmail.com>
2025-10-27 22:46:46 +0900

    Delete weekly_amazon_us_plus.py

--- weekly_amazon_us_plus.py
+++
@@ -1,5 +0,0 @@
-# weekly_amazon_us_plus.py
-import os, json
-from agg_plus import run_weekly
-DATA_DIR = os.getenv("DATA_DIR", "./data/daily")
-print(json.dumps({"amazon_us": run_weekly(DATA_DIR, "amazon_us", min_days=3)}, ensure_ascii=False, indent=2))
